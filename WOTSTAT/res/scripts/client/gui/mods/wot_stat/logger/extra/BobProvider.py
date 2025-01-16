d65035088d4a58ab36bbf9095022e69c1000116a
Andrei <soprachev@mail.ru>
2025-01-16 21:18:48 +0300

    fix bb

--- WOTSTAT/res/scripts/client/gui/mods/wot_stat/logger/extra/BobProvider.py
+++ WOTSTAT/res/scripts/client/gui/mods/wot_stat/logger/extra/BobProvider.py
@@ -2,26 +2,28 @@ import BigWorld
 
 from constants import ARENA_BONUS_TYPE
 from helpers import dependency
+from PlayerEvents import g_playerEvents
 
 from .IExtraProvider import IExtraProvider
 from ...common.exceptionSending import with_exception_sending
 
-
 from skeletons.gui.game_control import IBobController
 from skeletons.gui.battle_session import IBattleSessionProvider
-from skeletons.gui.game_control import IBobController
 from gui.bob.bob_helpers import getShortSkillName
 from gui.battle_control.controllers.bob_ctrl import BattleBobController
-
+from skeletons.gui.shared import IItemsCache
 
 class BobProvider(IExtraProvider):
   sessionProvider = dependency.descriptor(IBattleSessionProvider) # type: IBattleSessionProvider
   bobCtrl = dependency.descriptor(IBobController) # type: IBobController
+  itemsCache = dependency.descriptor(IItemsCache) # type: IItemsCache
    
   def __init__(self):
     self.sessionProvider.onBattleSessionStart += self.onBattleSessionStart
     self.sessionProvider.onBattleSessionStop += self.onBattleSessionStop
     self.bobCtrl.onUpdated += self.onUpdated
+    self.bobCtrl.onTokensUpdated += self.onUpdated
+    g_playerEvents.onClientUpdated += self.onUpdated
     
     self.allySkill = None
     self.enemySkill = None
@@ -30,6 +32,8 @@ class BobProvider(IExtraProvider):
     self.personalLevel = 0
     self.stats = {}
     self.isInited = False
+    
+    self.onUpdated()
     
   @with_exception_sending
   def onBattleSessionStart(self):
@@ -57,6 +61,8 @@ class BobProvider(IExtraProvider):
     dynamicBob.onSkillUpdated -= self.onUpdated
     dynamicBob.onInited -= self.onUpdated
     
+    BigWorld.callback(1, self.onUpdated)
+    
   @with_exception_sending
   def onUpdated(self, *a, **kw):
     dynamicBob = self.sessionProvider.dynamic.bob # type: BattleBobController
@@ -66,7 +72,9 @@ class BobProvider(IExtraProvider):
       self.allyBloggerId = dynamicBob.getAllyBloggerID()
       self.enemyBloggerId = dynamicBob.getEnemyBloggerID()
       
-    self.personalLevel = self.bobCtrl.personalLevel
+    if (self.itemsCache.isSynced()):
+      self.personalLevel = self.bobCtrl.personalLevel
+
     self.stats = {}
     for t in self.bobCtrl.teamsRequester.getTeamsList():
       self.stats[str(t.team)] = { 'score': t.score, 'rank': t.rank }
