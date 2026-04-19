c25a2c96b16f55aba47ce89f2e2c9ea58ead2837
Andrei Soprachev <13734096+SoprachevAK@users.noreply.github.com>
2026-04-19 03:05:28 +0300

    listen leaderboard position changing

--- WOTSTAT/res/scripts/client/gui/mods/wot_stat/logger/loggers/comp7Logger.py
+++ WOTSTAT/res/scripts/client/gui/mods/wot_stat/logger/loggers/comp7Logger.py
@@ -6,11 +6,25 @@ from gui.ClientUpdateManager import g_clientUpdateManager
 from helpers import dependency
 from skeletons.gui.shared import IItemsCache
 from skeletons.gui.game_control import IComp7Controller
+from gui.game_control.comp7_controller import _LeaderboardDataProvider
+from Event import SafeEvent
 
 from ..eventLogger import eventLogger
 from ..events import OnComp7Info
 from ..utils import setup_hangar_event
 from ...common.exceptionSending import with_exception_sending
+
+onOwnDataGet = SafeEvent()
+getOwnData_old = _LeaderboardDataProvider.getOwnData
+
+@adisp.adisp_async
+@adisp.adisp_process
+def getOwnData(self, callback):
+  result = yield getOwnData_old(self)
+  onOwnDataGet(result)
+  callback(result)
+
+_LeaderboardDataProvider.getOwnData = getOwnData
 
 class Comp7Logger:
   
@@ -18,14 +32,17 @@ class Comp7Logger:
   comp7Controller = dependency.descriptor(IComp7Controller) # type: IComp7Controller
   
   def __init__(self):
+    global onOwnDataGet
     self._lastEliteTrashload = None
     self._lastRating = None
     self._leaderboardPosition = None
+    self._leaderboardLoading = False
 
     self.itemsCache.onSyncCompleted += self.onItemsCacheSyncCompleted
     self.comp7Controller.onRankUpdated += self.onComp7RankUpdated
     g_clientUpdateManager.addCallbacks({'cache.entitlements': self.onEntitlementsUpdated})
     ServicesLocator.appLoader.onGUISpaceEntered += self.onGUISpaceEntered
+    onOwnDataGet += self.onOwnDataGet
 
   @with_exception_sending
   def getSeasonName(self):
@@ -65,15 +82,30 @@ class Comp7Logger:
   def onGUISpaceEntered(self, *a, **k):
     self.onChanged()
 
+  @with_exception_sending
+  def onOwnDataGet(self, result=None):
+    # type: (_LeaderboardDataProvider._OwnData) -> None
+    if self._leaderboardLoading: return
+    if not result: return
+    if self._leaderboardPosition == result.position: return
+
+    BigWorld.callback(0, self.onChanged)
+
   @adisp.adisp_process
   def onChanged(self):
     season = self.getSeasonName()
     if season is None: return
 
-    eliteTrashload, status = yield self.comp7Controller.leaderboard.getLastEliteRating()
+    leaderboard = self.comp7Controller.leaderboard # type: _LeaderboardDataProvider
+
+    self._leaderboardLoading = True
+    eliteTrashload, status = yield leaderboard.getLastEliteRating()
+    self._leaderboardLoading = False
     if not status: return
 
-    ownData = yield self.comp7Controller.leaderboard.getOwnData()
+    self._leaderboardLoading = True
+    ownData = yield leaderboard.getOwnData() # type: _LeaderboardDataProvider._OwnData
+    self._leaderboardLoading = False
     if ownData is None: return
     leaderboardPosition = ownData.position if ownData.isSuccess else None
 
