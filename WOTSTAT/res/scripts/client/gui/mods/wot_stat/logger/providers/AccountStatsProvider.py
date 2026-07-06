1631986c69bf0d6aa27282cb20e01257dce71bb7
Andrei Soprachev <13734096+SoprachevAK@users.noreply.github.com>
2026-07-06 04:30:06 +0300

    prepare for 1.44

--- WOTSTAT/res/scripts/client/gui/mods/wot_stat/logger/providers/AccountStatsProvider.py
+++ WOTSTAT/res/scripts/client/gui/mods/wot_stat/logger/providers/AccountStatsProvider.py
@@ -4,6 +4,7 @@ from helpers import dependency
 from skeletons.gui.shared import IItemsCache
 
 from ...common.exceptionSending import with_exception_sending
+from ...utils import print_error
 
 class AccountStatsProvider():
   
@@ -16,5 +17,18 @@ class AccountStatsProvider():
   @with_exception_sending
   def onSyncCompleted(self, *a, **k):
     if not self.itemsCache.isSynced(): return
-    mapsBlackListId = [ mapId for mapId, _ in self.itemsCache.items.stats.getMapsBlackList() ]
+    maps = self.itemsCache.items.stats.getMapsBlackList()
+
+    if maps is None:
+      self.mapBlackList = []
+      return
+    
+    if isinstance(maps, list):
+      mapsBlackListId = [ mapId for mapId, _ in maps ]
+    elif isinstance(maps, dict):
+      mapsBlackListId = [ mapID for _, _, mapID, _, _ in maps.values() ]
+    else:
+      print_error("Invalid type for mapBlackList")
+      self.mapBlackList = []
+
     self.mapBlackList = [ ArenaType.g_geometryCache[mapId].geometryName if mapId in ArenaType.g_geometryCache else None for mapId in mapsBlackListId ]
